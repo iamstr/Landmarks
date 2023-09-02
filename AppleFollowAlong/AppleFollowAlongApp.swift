7aa303c67fb19fb09cbb8dbc062c92f3f457719e
iamsatar <abdisatar.mohamed@pixeledge.io>
2023-09-03 00:35:27 +0300

    Initial Commit

---
+++ AppleFollowAlong/AppleFollowAlongApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFollowAlongApp.swift
+//  AppleFollowAlong
+//
+//  Created by Abdisatar Mohamed on 03/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppleFollowAlongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
