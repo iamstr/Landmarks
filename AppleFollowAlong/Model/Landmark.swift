c66146fbacf99e847aff83c1ab97253e17b33e67
iamsatar <abdisatar.mohamed@pixeledge.io>
2023-10-25 19:02:18 +0300

    feat: add computed value for images

--- AppleFollowAlong/Model/Landmark.swift
+++ AppleFollowAlong/Model/Landmark.swift
@@ -1,36 +1,32 @@
-//
-//  Landmark.swift
-//  AppleFollowAlong
-//
-//  Created by Abdisatar Mohamed on 03/09/2023.
-//
-
 import Foundation
 import SwiftUI
 import CoreLocation
 
-struct Landmark: Hashable, Codable {
+
+struct Landmark: Hashable, Codable, Identifiable {
     var id: Int
     var name: String
     var park: String
     var state: String
     var description: String
-    
-    
+    var isFavorite: Bool
+
     private var imageName: String
-       var image: Image {
-           Image(imageName)
-       }
-    
+    var image: Image {
+        Image(imageName)
+    }
+
+
     private var coordinates: Coordinates
     var locationCoordinate: CLLocationCoordinate2D {
-           CLLocationCoordinate2D(
-               latitude: coordinates.latitude,
-               longitude: coordinates.longitude)
-       }
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
 
-        struct Coordinates: Hashable, Codable {
-            var latitude: Double
-            var longitude: Double
-        }
+
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
 }
